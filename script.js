b2fcae769334552a04e21208464d0525ba47d003
Andra Strachinaru <andra.popa1989@gmail.com>
2018-07-01 12:31:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function animateIcon() {
+    var info = document.getElementById("info");
+    info.innerHTML = "&#xf129;";
+    
+    setTimeout(function(){
+        info.innerHTML = "&#xf05a;";
+    }, 1000);
+}
+
+animateIcon();
+
+setInterval(animateIcon, 4000);
+
+
